1d0fec4daf80bf5f8feb9fa8ec9b9bfd11258529
iwillkillb <iwillkillb@gmail.com>
2018-07-12 13:23:36 +0900

    2018.07.12. 캡슐화 정리

---
+++ day07-Encap/src/encap/person/Employee.java
@@ -0,0 +1,46 @@
+package encap.person;
+
+public class Employee extends Person {
+	// 1. 멤버변수 선언부
+	private String dept;
+	
+	// 2. 생성자 선언부
+	Employee () {
+		
+	}
+	
+	Employee (String dept) {
+		this.dept = dept;
+	}
+
+	Employee (String id, String name, int age, String dept) {
+		super (id, name, age);
+		this.dept = dept;
+	}
+	
+	
+	
+	// dept 필드 접근자
+	public String getDept() {
+		return dept;
+	}
+	// dept 필드 수정자
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	
+	
+	
+	// 3. 메소드 선언부
+	@Override
+	public String toString() {
+		String employeeStr = String.format(", 부서 : %s", dept);
+		return "직원 정보 - " + super.toString() + employeeStr;
+	}
+
+	@Override
+	public void print() {
+		System.out.println("== 직원 ==");
+		System.out.println(this);
+	}
+}
---
+++ day07-Encap/src/encap/person/Teacher.java
@@ -0,0 +1,48 @@
+package encap.person;
+
+public class Teacher extends Person {
+	// 1. 멤버변수 선언부
+	private String subject;
+
+	// 2. 생성자 선언부
+	Teacher () {
+		
+	}
+	
+	Teacher (String subject) {
+		this.subject = subject;
+	}
+	
+	Teacher (String id, String name, int age, String subject) {
+		super (id, name, age);
+		this.subject = subject;
+	}
+
+	
+	
+	// subject 필드 접근자
+	public String getSubject() {
+		return subject;
+	}
+	// subject 필드 수정자
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	
+	
+	
+	// 3. 메소드 선언부
+	@Override
+	public String toString() {
+		String teacherStr = String.format(", 교과 : %s", subject);
+		return "교사 정보 - " + super.toString() + teacherStr;
+	}
+
+	@Override
+	public void print() {
+		System.out.println("== 교사 ==");
+		System.out.println(this);
+	}
+	
+}

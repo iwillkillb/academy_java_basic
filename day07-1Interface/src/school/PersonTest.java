590f674297b62bf6edeec1eaac5ae918f7778b0a
iwillkillb <iwillkillb@gmail.com>
2018-07-12 14:50:07 +0900

    2018.07.12. 인터페이스

---
+++ day07-1Interface/src/school/PersonTest.java
@@ -0,0 +1,24 @@
+package school;
+/**
+ * Person과 상속관계인 Student를 테스트하는 클래스.
+ * @author PC38208
+ *
+ */
+public class PersonTest {
+
+	public static void main(String[] args) {
+		// 1. 선언
+		Person[] persons = new Person[3];
+		persons[0] = new Student ("S01", "홍길동", 18, "도술");
+		persons[1] = new Teacher ("T02", "이몽룡", 36, "과거시험");
+		persons[2] = new Employee ("E03", "심청", 20, "인당수");
+		
+		// 2. 초기화
+		
+		// 3. 사용
+		for (Person person: persons) {
+			person.print();
+		}
+	}
+
+}
---
+++ day07-1Interface/src/school/Teacher.java
@@ -0,0 +1,59 @@
+package school;
+
+public class Teacher extends Person implements Lesson {
+	// 1. 멤버변수 선언부
+	private String subject;
+
+	// 2. 생성자 선언부
+	Teacher () {
+		
+	}
+	
+	Teacher (String subject) {
+		this.subject = subject;
+	}
+	
+	Teacher (String id, String name, int age, String subject) {
+		super (id, name, age);
+		this.subject = subject;
+	}
+
+	
+	
+	// subject 필드 접근자
+	public String getSubject() {
+		return subject;
+	}
+	// subject 필드 수정자
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	
+	
+	
+	// 3. 메소드 선언부
+	@Override
+	public String toString() {
+		String teacherStr = String.format(", 교과 : %s", subject);
+		return "교사 정보 - " + super.toString() + teacherStr;
+	}
+
+	@Override
+	public void print() {
+		System.out.println("== 교사 ==");
+		System.out.println(this);
+	}
+
+	@Override
+	public String attend() {
+		String tchStr = String.format("교사 [%s](이)가 출근하였습니다.", this.getId());
+		return tchStr;
+	}
+
+	@Override
+	public String lesson() {
+		return String.format("교사 [%s](이)가 [%s] 수업을 하고 있습니다.", this.getId(), this.getSubject());
+	}
+	
+}
